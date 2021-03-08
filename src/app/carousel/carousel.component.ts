5872f06298a2f3838afeb59f806a9b8337169984
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-03-08 23:11:32 +0100

    Fix carousel suite erreur merge + click sur image vers la vue

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { PretatousService } from '../service/pretatous.service';
+import {Component, OnInit} from '@angular/core';
+import {PretatousService} from '../service/pretatous.service';
+import {Router, ActivatedRoute} from '@angular/router';
 
-@Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
-})
 
+@Component({selector: 'app-carousel', templateUrl: './carousel.component.html', styleUrls: ['./carousel.component.css']})
 
 
 export class CarouselComponent implements OnInit {
 
-  productList : any;
+    productList : any;
 
-  constructor(private pretatousService: PretatousService) { }
+    constructor(private pretatousService : PretatousService,
+      private router : Router, 
+      private route : ActivatedRoute) {}
 
-  chunks(array: any, size: number) {
-    let results = [];
-    results = [];
-    while (array.length) {
-      results.push(array.splice(0, size));
+    chunks(array : any, size : number) {
+        let results = [];
+        results = [];
+        while (array.length) {
+            results.push(array.splice(0, size));
+        }
+        return results;
     }
-    return results;
-  }
 
-  ngOnInit(): void {
-        this.pretatousService.getProducts(9).subscribe((data: any) => {
-        this.productList = this.chunks(data.data, 3);
-        console.log('data', data.data);
-      });
+    ngOnInit(): void {
+        this.pretatousService.getProducts(9).subscribe((data : any) => {
+            this.productList = this.chunks(data.data, 3);
+            console.log('data', data.data);
+        });
     }
-  }
 
+    goToView(id:String){
+      this.router.navigateByUrl('product/product-view/' + id);
+    }
+}
 
